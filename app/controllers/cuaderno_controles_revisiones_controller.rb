f4d0ef728e432260b7463df5281d00c8b41cd01c
dirceuml <dirceu.ml@gmail.com>
2012-07-13 17:05:33 -0500

    Revision de cuaderno de control

--- app/controllers/cuaderno_controles_revisiones_controller.rb
+++ app/controllers/cuaderno_controles_revisiones_controller.rb
@@ -1,11 +1,5 @@
 class CuadernoControlesRevisionesController < ApplicationController
-  before_filter :find_alumno
   
-  before_filter :find_cuaderno_control_revision, :only => [:show,
-                                                          :edit,
-                                                          :update,
-                                                          :destroy]
-
   # GET /cuaderno_controles_eventos
   # GET /cuaderno_controles_eventos.json
   def index
@@ -38,12 +32,12 @@ class CuadernoControlesRevisionesController < ApplicationController
       redirect_to(log_in_path) and return
     end
 
-    #    @cuaderno_control_revision = CuadernoControlEvento.find(params[:id])
-#
-#    respond_to do |format|
-#      format.html # show.html.erb
-#      format.json { render json: @cuaderno_control_revision }
-#    end
+    @cuaderno_control_revision = CuadernoControlRevision.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @cuaderno_control_revision }
+    end
   end
 
   # GET /cuaderno_controles_eventos/1/edit
@@ -52,7 +46,61 @@ class CuadernoControlesRevisionesController < ApplicationController
       redirect_to(log_in_path) and return
     end
 
-    #@cuaderno_control_revision = CuadernoControlEvento.find(params[:id])
+    @cuaderno_control_revision = CuadernoControlRevision.find(params[:id])
+  end
+
+  # GET /cuaderno_controles_eventos/new
+  # GET /cuaderno_controles_eventos/new.json
+  def new
+    if current_user.nil?
+      redirect_to(log_in_path) and return
+    end
+
+    @cuaderno_control_revision = CuadernoControlRevision.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @cuaderno_control_revision }
+    end
+  end
+  
+  # GET /cuaderno_controles_revisiones/1/revisar
+  def revisar
+    if current_user.nil?
+      redirect_to(log_in_path) and return
+    end
+
+    @cuaderno_control_revision = CuadernoControlRevision.find(params[:id])
+
+    respond_to do |format|
+      if @cuaderno_control_revision.update_attributes(:revisado => 1)
+        format.html { redirect_to @cuaderno_control_revision, notice: 'Cuaderno de control revisado satisfactoriamente.' }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to @cuaderno_control_revision, notice: 'Cuaderno de control no fue revisado.' }
+        format.json { render json: @cuaderno_control_revision.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # POST /cuaderno_controles_eventos
+  # POST /cuaderno_controles_eventos.json
+  def create
+    if current_user.nil?
+      redirect_to(log_in_path) and return
+    end
+
+    @cuaderno_control_revision = CuadernoControlRevision.new(params[:cuaderno_control_revision])
+    
+    respond_to do |format|
+      if @cuaderno_control_revision.save
+        format.html { redirect_to [@cuaderno_control_revision], notice: 'Revision agregada.' }
+        format.json { render json: [@cuaderno_control_revision], status: :created, location: @cuaderno_control_revision }
+      else
+        format.html { render action: "new", alert: "Revision no fue agregada." }
+        format.json { render json: @cuaderno_control_revision.errors, status: :unprocessable_entity }
+      end
+    end
   end
   
  
@@ -65,7 +113,7 @@ class CuadernoControlesRevisionesController < ApplicationController
 
     if @cuaderno_control_revision.update_attributes(params[:cuaderno_control_revision])
       flash[:notice] = "Revision actualizada."
-      redirect_to [@alumno, @cuaderno_control_revision]
+      redirect_to [@cuaderno_control_revision]
     else
       flash[:alert] = "Revision no fue actualizada."
       render :action => "edit"
@@ -80,68 +128,12 @@ class CuadernoControlesRevisionesController < ApplicationController
       redirect_to(log_in_path) and return
     end
 
-    #@cuaderno_control_revision = CuadernoControlEvento.find(params[:id])
-    @cuaderno_control_revision.destroy
+    @cuaderno_control_revision = CuadernoControlRevision.find(params[:id])
+    @cuaderno_control_revision.destroy    
     
-    redirect_to @alumno
-
-#    respond_to do |format|
-#      format.html { redirect_to @cuaderno_control }
-#      format.json { head :no_content }
-#    end
-  end
-
-  # GET /cuaderno_controles_eventos/new
-  # GET /cuaderno_controles_eventos/new.json
-  def new
-    if current_user.nil?
-      redirect_to(log_in_path) and return
-    end
-
-    #@cuaderno_control_revision = CuadernoControlEvento.new
-    @cuaderno_control_revision = @alumno.cuaderno_controles_revisiones.build
-
     respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @cuaderno_control_revision }
+      format.html { redirect_to @cuaderno_control }
+      format.json { head :no_content }
     end
   end
-
-  # POST /cuaderno_controles_eventos
-  # POST /cuaderno_controles_eventos.json
-  def create
-    if current_user.nil?
-      redirect_to(log_in_path) and return
-    end
-
-    #@cuaderno_control_revision = CuadernoControlEvento.new(params[:cuaderno_control_revision])
-    @cuaderno_control_revision = @alumno.cuaderno_controles_revisiones.build(params[:cuaderno_control_revision])
-    
-    if @cuaderno_control_revision.save
-      flash[:notice] = "Revision agregada."
-      redirect_to [@alumno, @cuaderno_control_revision]
-    else
-      flash[:alert] = "Revision no fue agregada."
-      render :action => "new"
-    end
-
-#    respond_to do |format|
-#      if @cuaderno_control_revision.save
-#        format.html { redirect_to [@cuaderno_control, @cuaderno_control_revision], notice: 'Evento agregado.' }
-#        format.json { render json: [@cuaderno_control, @cuaderno_control_revision], status: :created, location: @cuaderno_control_revision }
-#      else
-#        format.html { render action: "new", alert: "Evento no fué agregado." }
-#        format.json { render json: @cuaderno_control_revision.errors, status: :unprocessable_entity }
-#      end
-#    end
-  end
-  
-  private
-    def find_alumno
-      @alumno = Alumno.find_by_id(params[:alumno_id]) # _by_id -> para q no de error si no se accede via alumno (:alumno_id nil)
-    end
-    
-    def find_cuaderno_control_revision
-      @cuaderno_control_revision = @alumno.cuaderno_controles_revisiones.find(params[:id])
-    end 
 end
--- app/models/cuaderno_control_revision.rb
+++ app/models/cuaderno_control_revision.rb
@@ -3,5 +3,5 @@ class CuadernoControlRevision < ActiveRecord::Base
   belongs_to :persona_vinculada
   belongs_to :alumno
   
-  validates :cuaderno_control_id, :alumno_id, :persona_vinculada_id, :revisado, :usuario, :presence => true
+  validates :cuaderno_control_id, :alumno_id, :persona_vinculada_id, :revisado, :usuario, :presence => true  
 end
