ff2bfbe705e45fe231d772bcc3531ca39a77ce10
Dirceu Marquina Lazarte <dirceu.ml@gmail.com>
2012-07-19 15:42:29 -0500

    Validacion de usuarios

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -6,8 +6,8 @@ class Usuario < ActiveRecord::Base
   belongs_to :perfil
   
   #validates :perfil_id, :usuario, :nombre, :clave, :presence => true
-  validates :clave, :confirmation => true
-  validates :usuario, :uniqueness => { :scope => :colegio_id, :message => "El usuario ya esta registrado" }
+  #validates :clave, :confirmation => true
+  #validates :usuario, :uniqueness => { :scope => :colegio_id, :message => "El usuario ya esta registrado" }
   
   def self.authenticate(usuario, password)
     user = find_by_usuario(usuario)
