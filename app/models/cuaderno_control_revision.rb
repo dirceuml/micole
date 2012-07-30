4779d2e757c9252c3dd7f6c1fb21d4f5ed806f7f
dirceuml <dirceu.ml@gmail.com>
2012-07-29 19:46:00 -0500

    Ajustes varios

--- app/models/cuaderno_control_revision.rb
+++ app/models/cuaderno_control_revision.rb
@@ -6,6 +6,7 @@ class CuadernoControlRevision < ActiveRecord::Base
   validates :cuaderno_control_id, :alumno_id, :persona_vinculada_id, :revisado, :usuario, :presence => true  
   
   scope :revisado, where(:revisado => 1)
+  scope :no_revisado, where(:revisado => 0)
   scope :cerrado, joins(:cuaderno_control).where("cuadernos_controles.estado = 2")
   scope :abierto, joins(:cuaderno_control).where("cuadernos_controles.estado = 1")
   scope :se_revisan_por, lambda { |padre| joins(:alumno => :alumnos_personas_vinculadas).where("revisa_control = 1 and alumnos_personas_vinculadas.persona_vinculada_id = ?", padre) }
