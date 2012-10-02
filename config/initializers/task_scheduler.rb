24bb537a565b43047f71ef2ae9e28e081fd8737b
dirceuml <dirceu.ml@gmail.com>
2012-10-01 22:58:12 -0500

    Ajustes correo y actividades

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,7 +4,7 @@ scheduler.every("1m") do
   Actividad.find(5).enviar_recordatorio
 end 
 
-scheduler.cron '0 10 * * * Etc/GMT-5' do
-  # todos los días a las 10:00 en GMT-5
-  Actividad.find(5).enviar_recordatorio
-end
+#scheduler.cron '0 10 * * * Etc/GMT-5' do
+#  # todos los días a las 10:00 en GMT-5
+#  Actividad.find(5).enviar_recordatorio
+#end
