ac07a2b5410a90b6e6d43afc0a68e6b339e0bc48
Dirceu Marquina Lazarte <dirceu.ml@gmail.com>
2012-10-02 18:23:58 -0500

    Pruebas rufus

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,7 +4,8 @@ scheduler = Rufus::Scheduler.start_new
 #  Actividad.find(5).enviar_recordatorio
 #end 
 
-scheduler.cron '0 10 * * * Etc/GMT-5' do
+#scheduler.cron '0 10 * * * Etc/GMT-5' do
+scheduler.cron '28 23 * * *' do
   # todos los días a las 10:00 en GMT-5
   Actividad.find(5).enviar_recordatorio
 end
