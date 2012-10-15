ceee729e56cbdea7e402db966f81944eb7d0d257
dirceuml <dirceu.ml@gmail.com>
2012-10-15 00:25:45 -0500

    Scheduler

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,13 +1,13 @@
 scheduler = Rufus::Scheduler.start_new
 
-scheduler.every("1m") do
+#scheduler.every("1m") do
 #scheduler.cron '0 10 * * * Etc/GMT-5' do
-#scheduler.cron '0 15 * * *' do
+scheduler.cron '0 15 * * *' do
   # todos los días a las 10:00 en GMT-5 
-#  Actividad.pendiente(Date.current).find_each do |a|
-#    a.enviar_recordatorio
-#  end
-  puts "Funciona"
+  Actividad.pendiente(Date.current).find_each do |a|
+    a.enviar_recordatorio
+  end
+#  puts "Funciona"
 end
 
 #scheduler.join
