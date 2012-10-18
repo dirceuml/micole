c084cb40152f76d790d0bf6e90eed2197bebae4b
dirceuml <dirceu.ml@gmail.com>
2012-10-18 15:59:29 -0500

    Cambio de correo por bloqueo y de programacion para presentacion

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,8 +1,8 @@
 scheduler = Rufus::Scheduler.start_new
 
-scheduler.every("8h") do
+scheduler.every("20m") do
 #scheduler.cron '0 10 * * * Etc/GMT-5' do
-#scheduler.cron '0 20 * * *' do
+#scheduler.cron '0 15 * * *' do
   # todos los días a las 10:00 en GMT-5 
   Actividad.pendiente(Date.current).find_each do |a|
     a.enviar_recordatorio
