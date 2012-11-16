18b24eaef198c74505b583a2310d4d7db0662689
dirceuml <dirceu.ml@gmail.com>
2012-11-16 16:12:47 -0500

    Ajuste

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -33,5 +33,5 @@ ActiveSupport::Inflector.inflections do |inflect|
   inflect.irregular 'session', 'sessions'
   inflect.irregular 'actividad_seccion', 'actividades_secciones'
   inflect.irregular 'cuaderno_control_revision', 'cuaderno_controles_revisiones'
-  inflect.irregular 'ActividadSeccion', 'ActividadesSecciones'
+  #inflect.irregular 'ActividadSeccion', 'ActividadesSecciones'
 end
