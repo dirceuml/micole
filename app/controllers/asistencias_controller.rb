11385b10fd907d2d47295fdfd0c443e8e7d6f6ef
Dirceu Marquina Lazarte <dirceu.ml@gmail.com>
2014-05-04 00:15:14 -0500

    Ajustes en autorizaciones

--- app/controllers/asistencias_controller.rb
+++ app/controllers/asistencias_controller.rb
@@ -123,7 +123,7 @@ class AsistenciasController < ApplicationController
       render :new
     else    
       
-      if !Asistencia.find_by_id(codigo)
+      if !Alumno.find_by_id(codigo)
         flash[:notice] = "El codigo #{codigo} no existe"
         redirect_to(:new_asistencia) and return
       end
