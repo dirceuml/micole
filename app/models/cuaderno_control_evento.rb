0653bac366ae65f4ada9b6c8d73c0cd56b73d1d6
Edgar Gutierrez <edgar.gutierrez100@gmail.com>
2014-05-21 01:10:55 -0500

    tipos de eventos y c.control

--- app/models/cuaderno_control_evento.rb
+++ app/models/cuaderno_control_evento.rb
@@ -6,6 +6,7 @@ class CuadernoControlEvento < ActiveRecord::Base
   
   validates :cuaderno_control_id, :tipo_evento_id, :descripcion, :usuario, :presence => { :message => ": El campo no puede estar vacio" }
   validate :fecha_evento_valida
+  validate :alumno_valida
   
   # se ha actualizado estos scopes colocando el parametro "anioescolar", pero nadie los usa.
   scope :cerrado, lambda { |anioescolar| joins(:cuaderno_control => {:seccion => :grado}).where("grados.anio_escolar_id = ? and cuadernos_controles.estado = 2", anioescolar)}
@@ -18,8 +19,19 @@ class CuadernoControlEvento < ActiveRecord::Base
   def fecha_evento_valida
     if fecha_evento.present?
       if fecha_evento <= Date.current then
-        errors.add(:fecha_evento, "La fecha de evento no puede ser menor o igual a hoy.")
+        errors.add(:fecha_evento, ": La fecha de evento no puede ser menor o igual a hoy.")
+      end
+    else
+      if TipoEvento.find(tipo_evento_id).obligatorio_fecha == 1
+        errors.add(:fecha_evento, ": Ingrese la fecha de programacion.")
       end
     end
   end
+  
+  def alumno_valida
+    if TipoEvento.find(tipo_evento_id).alcance== 3 && alumno_id.nil?
+      errors.add(:alumno_id, ": Seleccione un alumno.")
+    end
+  end
+  
 end
