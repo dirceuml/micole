76eef1dc8a41ac0246065d9979aa80e395423f5e
Edgar Gutierrez <edgar.gutierrez100@gmail.com>
2014-05-28 17:40:17 -0500

    validacion en actividades secciones

--- app/controllers/actividades_controller.rb
+++ app/controllers/actividades_controller.rb
@@ -137,46 +137,60 @@ class ActividadesController < ApplicationController
     @actividad = Actividad.find(params[:id])
     estado_anterior = @actividad.estado
     
-    ActiveRecord::Base.transaction do
-    respond_to do |format|
-      if @actividad.update_attributes(params[:actividad])
-        if @actividad.estado == 2 and estado_anterior == 1 and @actividad.requiere_autorizacion == 1
-          if @actividad.alcance_colegio == 1
-            sql = '
-              Insert Into autorizaciones (actividad_id, alumno_id, usuario, created_at, updated_at)
-              (Select '+ params[:id].to_s + ' actividad_id, alumno_id, \'' + current_user.usuario + '\', current_date, current_date
-              From anios_alumnos 
-              Where anio_escolar_id = '+ anio_escolar.id.to_s+ ' and estado = 1)
-              Returning id'
-          else
-            sql = '
-              Insert Into autorizaciones (actividad_id, alumno_id, usuario, created_at, updated_at)
-              (Select s.actividad_id, a.alumno_id, \'' + current_user.usuario + '\', current_date, current_date
-              From actividades_secciones s, anios_alumnos a
-              Where s.actividad_id = ' + params[:id].to_s + '
-                And s.seccion_id = a.seccion_id and a.estado = 1)
-              Returning id'
-          end
-          
-          if Autorizacion.connection.insert(sql)
-            #envío de solicitud de autorización
-            @actividad.enviar_solicitud_autorizacion
-          
-            format.html { redirect_to @actividad, notice: 'Actividad fue actualizada satisfactoriamente.' }
-            format.json { head :no_content }
-          else
-            format.html { redirect_to @actividad, notice: 'Actividad no fue actualizada.' } #OJO
-            format.json { render json: @actividad.errors, status: :unprocessable_entity }
-          end
-        else
-          format.html { redirect_to @actividad, notice: 'Actividad fue actualizada satisfactoriamente.' }
-          format.json { head :no_content }
+    validaok = 1
+    if !params[:actividad][:estado].nil?
+      if params[:actividad][:estado].to_i == 2 and estado_anterior == 1 and @actividad.alcance_colegio == 0
+        if ActividadSeccion.find_by_actividad_id(params[:id]).nil?
+          validaok = 0
         end
-      else
-        format.html { redirect_to @actividad, notice: 'Actividad no fue actualizada.' }
-        format.json { render json: @actividad.errors, status: :unprocessable_entity }
       end
     end
+    
+    if validaok == 0
+      flash[:notice] = 'Seleccione las secciones que participan en la actividad'
+      redirect_to(:actividad) and return
+    else
+      ActiveRecord::Base.transaction do
+        respond_to do |format|
+          if @actividad.update_attributes(params[:actividad])
+            if @actividad.estado == 2 and estado_anterior == 1 and @actividad.requiere_autorizacion == 1
+              if @actividad.alcance_colegio == 1
+                sql = '
+                  Insert Into autorizaciones (actividad_id, alumno_id, usuario, created_at, updated_at)
+                  (Select '+ params[:id].to_s + ' actividad_id, alumno_id, \'' + current_user.usuario + '\', current_date, current_date
+                  From anios_alumnos 
+                  Where anio_escolar_id = '+ anio_escolar.id.to_s+ ' and estado = 1)
+                  Returning id'
+              else
+                sql = '
+                  Insert Into autorizaciones (actividad_id, alumno_id, usuario, created_at, updated_at)
+                  (Select s.actividad_id, a.alumno_id, \'' + current_user.usuario + '\', current_date, current_date
+                  From actividades_secciones s, anios_alumnos a
+                    Where s.actividad_id = ' + params[:id].to_s + '
+                    And s.seccion_id = a.seccion_id and a.estado = 1)
+                  Returning id'
+              end
+              
+              if Autorizacion.connection.insert(sql)
+                #envío de solicitud de autorización
+                @actividad.enviar_solicitud_autorizacion
+                
+                format.html { redirect_to @actividad, notice: 'Actividad fue actualizada satisfactoriamente.' }
+                format.json { head :no_content }
+              else
+                  format.html { redirect_to @actividad, notice: 'Actividad no fue actualizada.' } #OJO
+                format.json { render json: @actividad.errors, status: :unprocessable_entity }
+              end
+            else
+              format.html { redirect_to @actividad, notice: 'Actividad fue actualizada satisfactoriamente.' }
+              format.json { head :no_content }
+            end
+          else
+            format.html { redirect_to @actividad, notice: 'Actividad no fue actualizada.' }
+            format.json { render json: @actividad.errors, status: :unprocessable_entity }
+          end
+        end
+      end
     end
   end
 
